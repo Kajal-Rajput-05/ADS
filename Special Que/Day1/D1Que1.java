2003bbbd14518b0c392e614ec149b1ea00f8e1bb
Kajal-Rajput-05 <161593122+Kajal-Rajput-05@users.noreply.github.com>
2024-04-08 10:26:13 +0530

    Add files via upload

---
+++ Special Que/Day1/D1Que1.java
@@ -0,0 +1,20 @@
+
+ public class D1Que1 {
+ 
+    static int gcd(int a, int b)
+    {
+        int result = Math.min(a, b);
+        while (result > 0) {
+            if (a % result == 0 && b % result == 0) {
+                break;
+            }
+            result--;
+        }
+        return result;
+    }
+ 
+    public static void main(String[] args){
+        int a = 11, b = 22;
+        System.out.print("GCD of " + a + " and " + b + " is " + gcd(a, b));
+    }
+}
---
+++ Special Que/Day1/D1Que2.java
@@ -0,0 +1,14 @@
+
+class D1Que2 {
+   
+    public static void main(String[] args){
+        int a = 11, b = 22;
+        int ans = (a > b) ? a : b;
+        while (true) {
+            if (ans % a == 0 && ans % b == 0)
+                break;
+            ans++;
+        }
+        System.out.println("LCM of " + a + " and " + b + " : " + ans);
+    }
+}
